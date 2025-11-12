6691927baf6c6f5c129e8015146b70acd54400b6
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-12 13:17:54 +0100

    premeros ejercicios de python

---
+++ Prova 1.py
@@ -0,0 +1,19 @@
+a = int(input("Escriure el primer nombre: "))
+b = int(input("Escriure el segon nombre: "))
+c = a + b 
+print("El resultat de la suma {} + {} és {}".format(a, b, c))
+c = a * b
+print("El resultat de la multiplicacion {} * {} és {}".format(a, b, c))
+
+a = float(input("Escriure el primer nombre: "))
+b = float(input("Escriure el segon nombre: "))
+c = a + b 
+print("El resultat de la suma {} + {} és {}".format(a, b, c))
+c = a * b
+print("El resultat de la multiplicacion {} * {} és {}".format(a, b, c))
+
+a = int(input("Escriure el primer nombre: "))
+b = int(input("Escriure el segon nombre: "))
+c = int(input("Escriure el tercer nombre: "))
+d = a + b + c
+print("El resultat de la suma {} + {} + {} es {}".format(a, b, c, d))

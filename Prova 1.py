b86bc1bd6dc41a8006dc85e3c9d0f6fdc3b4130e
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-12 13:39:06 +0100

    ex 1 i mes

--- Prova 1.py
+++ Prova 1.py
@@ -17,3 +17,19 @@ b = int(input("Escriure el segon nombre: "))
 c = int(input("Escriure el tercer nombre: "))
 d = a + b + c
 print("El resultat de la suma {} + {} + {} es {}".format(a, b, c, d))
+
+a = int(input("Escriure el primer nombre: "))
+b = int(input("Escriure el segon nombre: "))
+c = a + b 
+print("El resultat de la suma {} + {} és {}".format(a, b, c))
+if c > 20:
+    print("El resultat de la suma {} + {} és mayor que 20".format(a, b))
+else:
+    print("El resultat de la suma {} + {} és menor que 20".format(a, b))
+
+c = a * b
+print("El resultat de la multiplicacion {} * {} és {}".format(a, b, c))
+if c > 100:
+    print("El resultat de la multiplicacio {} + {} és mayor que 100".format(a, b))
+else:
+    print("El resultat de la multiplicacio {} + {} és menor que 100".format(a, b))

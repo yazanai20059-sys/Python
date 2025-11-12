60a5bd5c3d8cb5b20c6356cec3d5cc3b87e3afa0
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-12 14:01:50 +0100

    ex2

--- prova 2.py
+++ prova 2.py
@@ -1,2 +1,8 @@
 for i in range(10):
  print ("{} * 8 = {}".format(i, i*8))
+
+for i in range(10, 0, -1):
+ print ("{} * 8 = {}".format(i, i*8))
+
+for i in range(1, 10, 2):
+ print ("{} * 8 = {}".format(i, i*8))

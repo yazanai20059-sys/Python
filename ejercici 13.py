81740d4aca29b40207d7fc752490d37b89a6b36e
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-13 11:44:34 +0100

    ex 13 . a

--- ejercici 13.py
+++ ejercici 13.py
@@ -1,9 +1,16 @@
-x = float(input("Introdueix un nombre: "))
-y = float(input("Introdueix un nombre: "))
+x = int(input("Introdueix un nombre: "))
+y = int(input("Introdueix un nombre: "))
 z = x + y
+print("La suma de {} i {} és {}".format(x, y, z))
 q = x * y
+print("La multiplicacion de {} i {} és {}".format(x, y, q))
 w = x / y
+print("La division de {} i {} és {}".format(x, y, w))
 e = x - y
+print("La resta de {} i {} és {}".format(x, y, e))
 r = x ** y
+print("La elevacion de {} i {} és {}".format(x, y, r))
 t = x // y
+print("La raiz de {} i {} és {}".format(x, y, t))
 o = x % y
+print("el tanto por ciento de {} i {} és {}".format(x, y, o))

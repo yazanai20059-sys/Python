f5b533c0090ee047abc290ee327ec76452a2bc5f
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-19 12:49:57 +0100

    ex 19

---
+++ ejercicio 19.py
@@ -0,0 +1,12 @@
+def sumar_llista(lista):
+    return sum(lista) 
+def multiplicar_llista(lista):
+    resultado = 1  
+    for numero in lista:
+        resultado *= numero  
+    return resultado
+
+print(sumar_llista([5, 6, 7, 8])) 
+
+
+print(multiplicar_llista([1, 2, 3, 4]))  

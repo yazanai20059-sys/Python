983db584ad29224725cf21d082edb93931d9bd6c
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-19 13:34:51 +0100

    ex 26

---
+++ ejercici 26.py
@@ -0,0 +1,8 @@
+def gran_llista(llista):
+    return max(llista)
+
+
+print(gran_llista([3, 4, 2, 3, 10]))     # 10
+print(gran_llista([1, 5, 3, 9, 2]))      # 9
+print(gran_llista([-5, -2, -10, -1]))    # -1
+print(gran_llista([7]))                   # 7

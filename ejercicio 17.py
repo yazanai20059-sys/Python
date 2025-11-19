f3e0db12d582dbc83820a3c4ef848577ef1aa02c
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-19 12:27:32 +0100

    ex 17

---
+++ ejercicio 17.py
@@ -0,0 +1,17 @@
+def calcular_longitud(elemento):
+    return len(elemento)
+
+a = input("Ingrese el primer elemento: ")
+b = input("Ingrese el segundo elemento: ")
+c = input("Ingrese el tercer elemento: ")
+d = input("Ingrese el cuarto elemento: ")
+e = input("Ingrese el quinto elemento: ")
+f = input("Ingrese el sexto elemento: ")
+g = input("Ingrese el séptimo elemento: ")
+h = input("Ingrese el octavo elemento: ")
+i = input("Ingrese el noveno elemento: ")
+j = input("Ingrese el décimo elemento: ")
+
+mi_lista = [a, b, c, d, e, f, g, h, i, j]
+
+print("La longitud de la lista es:", calcular_longitud(mi_lista))

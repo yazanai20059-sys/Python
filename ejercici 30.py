a688d379f0d7d53451c2264f79a5036f7628a802
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-19 13:45:26 +0100

    ex 30

---
+++ ejercici 30.py
@@ -0,0 +1,9 @@
+def binari_a_enter(binari):
+    """Converteix un número binari (string) a enter"""
+    return int(binari, 2)
+
+
+print(binari_a_enter("1010"))     
+print(binari_a_enter("11111111"))  
+print(binari_a_enter("1"))         
+print(binari_a_enter("0"))        

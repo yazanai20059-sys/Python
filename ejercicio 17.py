42765324e2f8f3a90404e87cd9f1549e84c9766c
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-24 09:45:43 +0100

    ejercicio 17

--- ejercicio 17.py
+++ ejercicio 17.py
@@ -2,5 +2,7 @@ def calcular_longitud(elemento):
     return len(elemento)
 
 mi_lista = [1, 2, 3, 4, 5]
-
 print("La longitud de la lista es:", calcular_longitud(mi_lista))
+b="Hola, soc una persona"
+print("La longitud de la cadena es:", calcular_longitud(b))
+c=

e136e5c849a35a7d595d364587e219f99d05ac6e
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-24 09:46:55 +0100

    ex 46

--- ejercici 46.py
+++ ejercici 46.py
@@ -2,4 +2,4 @@ a=int(input("Ingrese el primer numero: "))
 if a%2==0:
     print("El numero {} es par".format(a))
 else:
-    print("El numero {} es impar".format(a))
+    print("El numero {} es impar".format(a)) 

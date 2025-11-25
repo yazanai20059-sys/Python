2be43fa3de4af0bc774affa57013be43c99f878b
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-25 09:02:46 +0100

    ex 47

--- ejercicio 47.py
+++ ejercicio 47.py
@@ -1,4 +1,17 @@
-def eliminarcapicua():
-    lista = [1, 2, 3, 2, 1, 4, 5, 5, 4]
-    print("Lista original:", lista)
+def eliminarcapicua(llista):
+    """
+    Elimina el primer i el darrer element d'una llista.
     
+    Paràmetres:
+        llista: llista d'elements
+    
+    Retorna:
+        Nova llista sense el primer i darrer element
+    """
+    if len(llista) <= 2:
+        return []
+    else:
+        return llista[1:-1]
+    
+llista = ["h", "o", "l", "a"]
+print(eliminarcapicua(llista))

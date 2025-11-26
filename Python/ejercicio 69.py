f9aef3219281b0ad82dfbb95a08ec5c1cb6da9a1
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-26 13:07:14 +0100

    ex

---
+++ Python/ejercicio 69.py
@@ -0,0 +1,12 @@
+def generar_potencies(potencia):
+    return [i ** potencia for i in range(10)]
+potencia_desejada = 3  
+resultat = generar_potencies(potencia_desejada)
+print(f'La llista dels 10 primers números elevats a la potència {potencia_desejada} és: {resultat}')    
+
+def generar_potencies(potencia):
+    return [i ** potencia for i in range(10)]
+potencia_desejada = 2
+resultat = generar_potencies(potencia_desejada)
+print(f'La llista dels 10 primers números elevats a la potència {potencia_desejada} és: {resultat}')
+

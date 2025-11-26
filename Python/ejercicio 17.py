01518664217ad33e08ec7b5943cf0a34514b4604
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-11-26 13:32:52 +0100

    ex

--- Python/ejercicio 17.py
+++ Python/ejercicio 17.py
@@ -5,4 +5,3 @@ mi_lista = [1, 2, 3, 4, 5]
 print("La longitud de la lista es:", calcular_longitud(mi_lista))
 b="Hola, soc una persona"
 print("La longitud de la cadena es:", calcular_longitud(b))
-c=

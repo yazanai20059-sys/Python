c29d3a05c47fc8312bb3094a829a37dec048e659
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-12-01 09:02:53 +0100

    prova

--- Python/provas personals.py
+++ Python/provas personals.py
@@ -1,1 +1,28 @@
 v1 = int(input("intruduix un nombra: "))
+while(v1>=5 and v1<=10) or (v1>=15 and v1<=20):
+    v1 = int(input("intruduix un nombra: "))
+    print(v1)
+print("fi del programa")
+    
+
+
+
+
+
+
+
+
+
+"""def ordenar(x,y):
+    if x < y:
+        return x, y
+    elif x > y:
+        return y, x
+    else:
+        return y, x
+    
+v1 = int(input("intruduix un nombra: "))
+v2 = int(input("intruduix un nombra: "))
+v1, v2 = ordenar(v1, v2)
+for e in range(v1+1, v2):
+    print(e) """

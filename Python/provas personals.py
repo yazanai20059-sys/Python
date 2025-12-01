0417da0036950a7b6c495725018f2b224f39567e
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-12-01 09:19:42 +0100

    prova

--- Python/provas personals.py
+++ Python/provas personals.py
@@ -1,8 +1,32 @@
 v1 = int(input("intruduix un nombra: "))
-while(v1>=5 and v1<=10 and v1!=6) or (v1>=15 and v1<=20 and v1!=16) or (v1>=25 and v1<=30 and v1!=26):
+while ((v1 >= 1 and v1 <= 1000)and (v1 % 2 == 1)):
     v1 = int(input("intruduix un nombra: "))
     print(v1)
 print("fi del programa")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""v1 = int(input("intruduix un nombra: "))
+while(v1>=5 and v1<=10 and v1!=6) or (v1>=15 and v1<=20 and v1!=16) or (v1>=25 and v1<=30 and v1!=26):
+    v1 = int(input("intruduix un nombra: "))
+    print(v1)
+print("fi del programa")"""
     
 
 

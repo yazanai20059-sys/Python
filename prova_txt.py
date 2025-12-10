c5b6b4171e6e265124a833d3741801436359017a
yazanai20059-sys <yazanai20059@iesjoanramis.org>
2025-12-10 14:02:47 +0100

    p

--- prova_txt.py
+++ prova_txt.py
@@ -1,6 +1,15 @@
-"""f = open("prova111.txt","r")
-print(f.read())
-f.close()"""
-
+l = [50, 100, 200]
+with open("prova111.txt","a") as f:
+    f.write(str(l))
 with open("prova111.txt","r") as f:
-    print(f.read(4))
+    lineas = f.readlines() 
+    lineas = [n[:-1] for n in lineas]
+    print(lineas)
+ 
+ 
+ 
+"""
+with open("prova111.txt","r") as f:
+    lineas = f.readlines() 
+    lineas = [n[:-1] for n in lineas]
+    print(lineas)"""

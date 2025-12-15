dc195abe28784b62032c0e95bcbb0831ab097a4b
yazanai20059-sys <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git>
2025-12-15 09:28:50 +0100

    p

---
+++ prova16.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+print(os.listdir("/home/hola"))
+
+
+
+"""
+if os.path.isfile("ex1.json"):
+    os.rename("ex1.json","ex2.json")
+    with open("ex2.json", "r") as f:
+        dades=json.load(f)
+        print(dades)
+else:
+    print("el fitxer no ecsisteix")"""

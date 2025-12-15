5c44c56421d839d6d8dc404e0163d1e277078260
yazanai20059-sys <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git>
2025-12-15 09:17:38 +0100

    p

--- important fitchers CSV y JSON/prova15.py
+++ important fitchers CSV y JSON/prova15.py
@@ -3,7 +3,7 @@ with open("ex2.json", "r") as f:
     dades=json.load(f)
     print(dades)
 
-with open("ex2.json", "w+") as f:
+with open("ex2.json", "w") as f:
     dades["colors"]=["blanc","negre"]
     json.dump(dades,f)
 
